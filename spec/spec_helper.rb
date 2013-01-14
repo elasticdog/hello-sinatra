20f17b7c3e2d3066d833e6cace647e099c4ecde1
Jonas Mueller <muellerj@me.com>
2013-01-14 15:16:54 +0100

    Augment test output with Turn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,19 @@
+ENV['RACK_ENV'] = 'test'
+
 require_relative '../my_app'
 require 'minitest/autorun'
-require 'minitest/pride'
+require "turn/autorun"
 require 'rack/test'
 
-set :environment, :test
-
+# Include rack helpers and expose full application stack
 class MiniTest::Spec
   include Rack::Test::Methods
   
   def app
-    Sinatra::Application
+    Rack::Builder.parse_file(File.dirname(__FILE__) + '/../config.ru').first
   end
 end
+
+# Configure test output
+# possible values are :pretty, :dot, :cue, :marshal, :outline, :progress
+Turn.config.format = :dot
